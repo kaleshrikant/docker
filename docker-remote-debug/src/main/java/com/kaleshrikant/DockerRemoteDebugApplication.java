7250991d34707cc763d4f693a6e5873db3dee565
Shrikant Kale <58430952+kaleshrikant@users.noreply.github.com>
2025-08-09 13:10:41 +0530

    initial commit

---
+++ docker-remote-debug/src/main/java/com/kaleshrikant/DockerRemoteDebugApplication.java
@@ -0,0 +1,13 @@
+package com.kaleshrikant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerRemoteDebugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerRemoteDebugApplication.class, args);
+	}
+
+}
