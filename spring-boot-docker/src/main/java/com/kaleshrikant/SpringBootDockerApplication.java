9b987fe2fb7cda1caf43674ed64e9c743b03c93f
Shrikant Kale <58430952+kaleshrikant@users.noreply.github.com>
2025-08-10 12:32:14 +0530

    added apis

--- spring-boot-docker/src/main/java/com/kaleshrikant/SpringBootDockerApplication.java
+++ spring-boot-docker/src/main/java/com/kaleshrikant/SpringBootDockerApplication.java
@@ -2,9 +2,17 @@ package com.kaleshrikant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootDockerApplication {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to Application..!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDockerApplication.class, args);
