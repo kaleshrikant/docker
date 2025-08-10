d35a92ac1868faf7e17c6407428457bf1e2b7557
Shrikant Kale <58430952+kaleshrikant@users.noreply.github.com>
2025-08-10 14:20:26 +0530

    Initial Commit

---
+++ Spring-Boot-Docker-Gradle-Plugin/src/main/java/com/kaleshrikant/SpringBootDockerGradlePluginApplication.java
@@ -0,0 +1,13 @@
+package com.kaleshrikant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerGradlePluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerGradlePluginApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-Docker-Gradle-Plugin/src/test/java/com/kaleshrikant/SpringBootDockerGradlePluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaleshrikant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerGradlePluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
