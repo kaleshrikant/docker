1b29f50a80dfd660f2b8ec68f3684cee0d913d44
Shrikant Kale <58430952+kaleshrikant@users.noreply.github.com>
2025-08-10 19:43:53 +0530

    API added

--- Spring-Boot-Docker-Google-Jib /src/main/java/com/kaleshrikant/SpringBootDockerGoogleJibApplication.java
+++ Spring-Boot-Docker-Google-Jib /src/main/java/com/kaleshrikant/SpringBootDockerGoogleJibApplication.java
@@ -2,9 +2,17 @@ package com.kaleshrikant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootDockerGoogleJibApplication {
+
+	@RequestMapping("/message")
+	public String message() {
+		return "Dockerize Java Application using Google-Jib..!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDockerGoogleJibApplication.class, args);
